3fe7510eae2bb4b8685e70fee7750fb991124130
Albinahmetaj <albingithub@gmail.com>
2022-08-28 23:28:20 +0200

    react icons and header css commited

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 import CTA from './CTA'
 import './header.css'
 import SelfImg from '../../assets/portfoliopic.png'
+import HeaderSocials from './HeaderSocials'
 
 function Header() {
   return (
     <header>
-      <div className="container header__container">
+      <div className="container header-container">
     <h5>Hej mitt namn är</h5>
     <h1>Albin Ahmetaj</h1>
     <h5 className='text-light'>Fullstack Utvecklare</h5>
     <CTA/>
+    <HeaderSocials/>
     <div className='self-portrait'>
-      <img src={SelfImg} alt="me" style={{width:'400px'}}/>
+      <img src={SelfImg} alt="me" style={{}}/>
       </div>
       <a href="#contact" className='scroll-down'>Scrolla Ner</a>
     
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import {FaLinkedin} from 'react-icons/fa'
+import {FaGithub} from 'react-icons/fa'
+import {FaDribbble} from 'react-icons/fa'
 
 function HeaderSocials() {
   return (
     <div className='header-socials'>
-        <a href="https://www.linkedin.com/in/albin-ahmetaj-3288241b5/" target="_blank"></a>
-        <a href="https://www.github.com" target="_blank"></a>
-        <a href="https://www.dribble.com" target="_blank"></a>
+        <a href="https://www.linkedin.com/in/albin-ahmetaj-3288241b5/" target="_blank"><FaLinkedin/></a>
+        <a href="https://github.com/Albinahmetaj?tab=repositories" target="_blank"><FaGithub/></a>
+        <a href="https://www.dribble.com" target="_blank"><FaDribbble/></a>
     </div>
   )
 }
