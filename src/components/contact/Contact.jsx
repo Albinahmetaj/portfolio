01de2c9caf7bd61a79b994cdb89daf02cbc62320
Albinahmetaj <albingithub@gmail.com>
2022-09-06 19:19:25 +0200

    contact and portfolio example commited

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
 import './contact.css'
+import {HiOutlineMail} from 'react-icons/hi';
+import {TbBrandMessenger} from 'react-icons/tb';
+import {FiPhoneCall} from 'react-icons/fi'
 
 function Contact() {
   return (
-    <section id="contact">Contact</section>
+    <section id="contact">
+      <h5>Låt Oss Ta Kontakt</h5>
+      <h2>Kontakta Mig</h2>
+
+      <div className="container contact-container">
+    <div className="contact-options">
+<article className="contact-option">
+  <HiOutlineMail className="contact-icon"/>
+  <h4>E-mail</h4>
+  <h5>Albin.Ahmetaj@hotmail.com</h5>
+  <a href="mailto:Albin.Ahmetaj@hotmail.com" target="_blank">Skicka ett meddelande</a>
+</article>
+<article className="contact-option">
+  <TbBrandMessenger className="contact-icon"/>
+  <h4>Messenger</h4>
+  <h5>Albin Ahmetaj</h5>
+  <a href="https://m.me/albin.ahmetaj.1" target="_blank">Skicka ett meddelande via Messenger</a>
+</article>
+<article className="contact-option">
+  <FiPhoneCall className="contact-icon"/>
+  <h4>Telefon</h4>
+  <h5>072-398 16 01</h5>
+  <a href="tel:+46723981601" target="_blank">Ring</a>
+</article>
+    </div>
+    <form action="">
+      <input type="text" name='name' placeholder="Ditt Fullständiga Famn" required/>
+      <input type="email" name='email' placeholder="Din E-Mail" required/>
+      <textarea name='message' rows="7" placeholder='Ditt Meddelande' required/>
+      <button type="submit" className="btn btn-primary">Skicka Meddelande</button>
+    </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,83 @@
 import React from 'react'
 import './portfolio.css'
+import GameImg from '../../assets/Spelkarta.PNG'
+
+
+const data=[
+{
+  id: 1,
+  image: GameImg,
+  title: '2d Spelutveckling',
+  github: 'https://github.com',
+  demo:'https://www.vimeo.com'
+},
+{
+  id: 2,
+  image: GameImg,
+  title: '2d Spelutveckling',
+  github: 'https://github.com',
+  demo:'https://www.vimeo.com'
+},
+{
+  id: 3,
+  image: GameImg,
+  title: '2d Spelutveckling',
+  github: 'https://github.com',
+  demo:'https://www.vimeo.com'
+},
+{
+  id: 4,
+  image: GameImg,
+  title: '2d Spelutveckling',
+  github: 'https://github.com',
+  demo:'https://www.vimeo.com'
+},
+{
+  id: 5,
+  image: GameImg,
+  title: '2d Spelutveckling',
+  github: 'https://github.com',
+  demo:'https://www.vimeo.com'
+},
+{
+  id: 6,
+  image: GameImg,
+  title: '2d Spelutveckling',
+  github: 'https://github.com',
+  demo:'https://www.vimeo.com'
+},
+]
+
 
 function Portfolio() {
   return (
-    <section id="portfolio">Portfolio</section>
+    <section id="portfolio">
+      <h5>Mina Projekt</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio-container">
+       {
+        data.map(({id, image, title, github, demo})=>{
+          return(
+            <article key={id} className='portfolio-item'>
+            <div className="portfolio-item-image">
+              <img src={image} alt={title}/>
+              
+            </div>
+            
+            <h3>{title}</h3>
+            <div className="portfolio-item-cta">
+            <a href={github} className='btn' target='_blank'>Github</a>
+            <a href={demo} className='btn btn-primary' target='_blank'>Demo Projekt</a>
+            </div>
+            </article>
+          )
+        })
+       }
+
+        
+      </div>
+    </section>
   )
 }
 
